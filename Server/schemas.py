2201e2781fb73ba2ba601f60b842a3741fdc22b6
MoumiDB <moumita.hait2010@gmail.com>
2026-02-14 00:19:47 +0530

    postgresql connection

---
+++ Server/schemas.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+
+class UserBase(BaseModel):
+    name: str
+    email: EmailStr
+
+class UserCreate(UserBase):
+    pass
+
+class UserResponse(UserBase):
+    id: int
+
+    class Config:
+        from_attributes = True
